a445e38396303d3c54027117fc2c6c70d8c837de
Vulpeshprajan <vulpeshprajan@gmail.com>
2021-07-24 12:54:44 +1000

    sidebar

--- src/componets/layout/DefaultLayout.js
+++ src/componets/layout/DefaultLayout.js
@@ -3,13 +3,17 @@ import Footer from "./partials/Footer";
 import Header from "./partials/Headers";
 import { Row, Container, Col } from "react-bootstrap";
 import "./defaultLayout.style.css";
+import SideBarNav from '../sidebar/SideBarNav';
 
 const Defaultlayout = ({ children }) => {
     return (
         <div className="default-layout">
     
                 
-                    <div className="left-bar" > I am from the left menu </div>
+                    <div className="left-bar" >
+                        <div className="admin-logo p-2 mb-5"> Admin panel</div>
+                    <SideBarNav/>    
+                    </div>
                
                     <div className="main" >
                         <Header />
---
+++ src/componets/sidebar/SideBarNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SideBarNav = () => {
+    return (
+<div className="sidebar-nav">
+
+            <ul>
+                <li>Dashboard</li>
+                <li>Category</li>
+                <li>Product</li>
+                <li>Order</li>
+                <li>Users</li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default SideBarNav
